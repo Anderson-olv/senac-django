57f4d42f0d0ad98202932118f77987325cb64a5d
Guest 01 <guest01@alunos.sc.senac.br>
2024-04-27 11:52:55 -0300

    Adicinando novo exercicio while python

---
+++ exerc_while.py
@@ -0,0 +1,59 @@
+# 01
+# numero = int(input("Digite um numero para saber a tabuada do mesmo: "))
+
+# multi = 1
+# while (multi <= 10):
+#     print(numero * multi)
+#     multi += 1
+
+# 02
+# pergunta = input("Você deseja somar dois números(sim ou nao)? ")
+
+# while pergunta == "sim":
+#     n1 = float(input("Digite o primeiro numero: "))
+#     n2 = float(input("Digite o segundo numero: "))
+#     print(f"Resultado da soma: {n1+n2}")
+#     pergunta = input("Você deseja somar dois números novamente(sim ou nao)? ")
+
+# 03
+# numero = int(input("Digite um numero para saber seu fatorial: "))
+
+# laco = numero - 1
+# fatorial = numero
+# while laco >= 1:
+#     fatorial = fatorial * laco
+#     laco = laco - 1
+# print(fatorial)
+
+# 04
+# par = 100
+# while par != 0:
+#     par = par - 2
+#     print(par)
+
+# 05
+# n1 = int(input("Digite um numero para saber se o numero é primo ou composto: "))
+
+# n_divisoes = 1
+# primo = n1
+# contador = 0
+# while n_divisoes <= n1:
+#     if n1 % n_divisoes == 0:
+#         contador += 1
+#     n_divisoes += 1
+
+# if n1 == 1:
+#     print("Esse numero não é primo e nem composto.")
+# elif contador == 2:
+#     print("Primo")
+# else:
+#     print("Composto")
+
+
+# 06
+# regressiva = 10
+
+# while regressiva > 0:
+#     print(regressiva)
+#     regressiva = regressiva - 1
+
